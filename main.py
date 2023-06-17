de05dd8987822f6c3bdf665025a3bf2b55f54214
Czadu <127044087+Czadu@users.noreply.github.com>
2023-06-17 14:27:37 +0200

    Kodilla_10_4_update

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
 from flask import Flask, render_template, request
 import csv
 import requests
+import tbdb_client
+
 
 app = Flask(__name__)
 
 
 
+
+### Stara wersja
+"""
 @app.route('/')
 def homepage():
-    movies = [
-        {'title' : 'Banana'}, {'title' : 'Mango'}, {'title' : 'Pineapple'}, 
-        {'title' : 'Kiwi'}, {'title' : 'Apple'}, {'title' : 'Plump'}, 
-        {'title' : 'Carrot'}, {'title' : 'Tomato'}, {'title' : 'Grape'}
-    ]
+    movies = tbdb_client.get_movies(how_many=8)
+    for movie in movies:
+        movie["poster_url"] = tbdb_client.get_poster_url(movie["poster_path"])
+        movie["backdrop_url"] = tbdb_client.get_backdrop_url(movie["backdrop_path"])
     return render_template("homepage.html", movies=movies)
+"""
+
+### Wersja z wyborem menu
+
+@app.route('/')
+def homepage():
+    valid_movie_list_types = ['now_playing', 'top_rated', 'upcoming', 'popular']
+    selected_list = request.args.get('list_type', 'popular')
+    if selected_list not in valid_movie_list_types:
+        selected_list = 'popular'
+    movies = tbdb_client.get_movies(how_many=8, list_type=selected_list)
+    for movie in movies:
+        movie["poster_url"] = tbdb_client.get_poster_url(movie["poster_path"])
+        movie["backdrop_url"] = tbdb_client.get_backdrop_url(movie["backdrop_path"])
+    return render_template("homepage.html", movies=movies, current_list=selected_list, movie_list_types=valid_movie_list_types)
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tbdb_client.get_single_movie(movie_id)
+    backdrop_path = details.get("backdrop_path")
+    if backdrop_path:
+        backdrop = tbdb_client.get_backdrop_url(backdrop_path)
+    else:
+        backdrop = None
+    cast = tbdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast, backdrop=backdrop)
+
+@app.context_processor
+def utility_processor():
+    def tbdb_image_url(profile_path, size):
+        base_url = "https://image.tmdb.org/t/p/"
+        return f"{base_url}{size}/{profile_path}"
+    return {"tbdb_image_url": tbdb_image_url}
+
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ tbdb_client.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request
+import requests
+
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDJkYzBjMjM2ZWZhOTY4M2Q1YzM4N2Q2YWY2OThiZiIsInN1YiI6IjY0NTc5MWYyNmFhOGUwMDBmZjVhNDljZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UGgXYvJYD9OyMF18OnkDxMcrE5bSwRBjdlKms7TS-6U"
+
+app = Flask(__name__)
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_poster_url(poster_api_path, size="w342" ):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
+
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
+    return data["results"][:how_many]
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tbdb_image_url": tmdb_image_url}
+
+###
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_backdrop_url(backdrop_path, size="w780"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}{backdrop_path}"
+
+### Obsada filmu
+
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def tbdb_image_url(profile_path, size):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{profile_path}"
+
+
+### Wsparcie dla wielu list filmów
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
